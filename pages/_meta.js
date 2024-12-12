4e80c9bf1e00cf5c4130947898daa2ebccc66e54
Kinda Hall <1142704468@qq.com>
2024-12-12 13:11:14 +0800

    feat: 🎸 初始化

---
+++ pages/_meta.js
@@ -0,0 +1,18 @@
+export default {
+    index: {
+        title: 'Home',
+        type: 'page'
+    },
+    github: {
+        title: 'Github',
+        type: 'page',
+        href: 'https://github.com/Alndaly/receiver-docs',
+        newWindow: true
+    },
+    control: {
+        title: 'Receiver',
+        type: 'page',
+        href: 'https://receiver.qingyon.com',
+        newWindow: true
+    }
+}
---
+++ theme.config.jsx
@@ -0,0 +1,18 @@
+export default {
+	logo: <span className='font-bold'>Receiver</span>,
+	project: {
+		link: 'https://github.com/Alndaly/receiver-docs',
+	},
+	darkMode: true,
+	footer: {
+		content: (
+			<span>
+				{new Date().getFullYear()} ©{' '}
+				<a href='https://qingyon.com' target='_blank'>
+					清韵科技（绍兴）有限公司
+				</a>
+				.
+			</span>
+		),
+	},
+};
